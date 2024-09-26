71d3d0384d2f6c89a1b9df5cfc012ca17d1d1065
brianveliz91 <bv14673@live.com>
2024-09-26 18:41:12 -0300

    createObjectUser

--- utils/createObjectUser.js
+++ utils/createObjectUser.js
@@ -1,13 +1,36 @@
+const createObjectUser = (args) => {
+    const [name, lastName, email, password] = args.slice(1);
+  
+    if (!name || !lastName || !email || !password) {
+        throw new Error("Missing data");
+    }
+  
+    return {
+        id: randomUUID(),
+        name,
+        lastName,
+        email,
+        password: hashedPassword,
+    };
+  };
+  
+  const createUpdateUserObject = (args) => {
+    const id = args[1];
+    const [name, lastName, email, password] = args.slice(2);
+  
+    if (!id) {
+        throw new Error("ID is missing");
+    }
+  
+    const updatedUser = {};
+    if (name) updatedUser.name = name;
+    if (lastName) updatedUser.lastName = lastName;
+    if (email) updatedUser.email = email;
+    if (password) updatedUser.password = password;
+  
+    updatedUser.id = id;
+  
+    return updatedUser;
+  };
 
-
-const createUserObject = (args) => {
-    try {
-        } catch (error) {}
-}
-
-const createUpdateUserObject = (args) => {
-    try {
-        } catch (error) {}
-}
-
-export {createUpdateUserObject,createUpdateUserObject}
+export {createUserObject, createUpdateUserObject};
