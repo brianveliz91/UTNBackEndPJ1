bb009518adfe82251c3d6193390c543881e79ca4
brianveliz91 <bv14673@live.com>
2024-09-26 16:12:25 -0300

    updateuser | deleteuser

--- models.js
+++ models.js
@@ -1,6 +1,10 @@
 import { existsSync, readFileSync, writeFileSync } from "node:fs"
 import { randomUUID } from "node:crypto"
 import { handleError } from "./utils/handleError.js"
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 
 const PATH_FILE_USER = process.env.PATH_FILE_USER;
 const PATH_FILE_ERROR = process.env.PATH_FILE_ERROR;
@@ -78,18 +82,63 @@ const addUser = (user) => {
     }
 };
 
+const updateUser = (id, dataUser) => {
+    try {
+        const { name, lastName, email, password } = dataUser;
 
+        if (!id) {
+            throw new Error("ID is missing");
+        }
 
-/*const updateUser(userData) => {
-    try{
-    } catch (error) {}
-}*/
+        const users = getUsers(PATH_FILE_USER);
+        const user = getUserById(id);
+
+        if (!user) {
+            throw new Error("User not found");
+        }
+
+        if (name) user.name = name;
+        if (lastName) user.lastName = lastName;
+        if (email) user.email = email;
+        if (password) user.password = password;
+
+        const userIndex = users.findIndex(u => u.id === id);
+        if (userIndex !== -1) {
+            users[userIndex] = user;
+        }
+
+        writeFileSync(PATH_FILE_USER, JSON.stringify(users));
+        return user;
+    } catch (error) {
+        const objError = handleError(error, PATH_FILE_ERROR);
+        return objError;
+    }
+};
+
+const userToUpdate = {
+    id: "11b8285f-a153-411c-850f-d37b20546386",
+    name: "PRUEBA",
+    lastName: "PEREZ",
+    email: "claudioveliz@example.com",
+    password: "David123"
+}
 
 const deleteUser = (id) => {
     try {
-    } catch (error) { }
-}
+        if (!id) {
+            throw new Error("ID is missing");
+        }
+        const users = getUsers(PATH_FILE_USER);
+        const user = getUserById(id);
 
-//export { getUsers, getUserById, addUser, updateUser, deleteUser };
-export { getUsers, getUserById, addUser, deleteUser }
+        const newUsers = users.filter((user) => user.id !== id);
+        writeFileSync(PATH_FILE_USER, JSON.stringify(newUsers));
 
+        return user;
+
+    } catch (error) {
+        const objError = handleError(error, PATH_FILE_ERROR);
+        return objError;
+    }
+};
+export { getUsers, getUserById, addUser, updateUser, deleteUser };
